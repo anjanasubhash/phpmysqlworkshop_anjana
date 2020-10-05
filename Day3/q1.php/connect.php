b6cb193f58001548762ddcc272a400f5172916d3
Anjana subhash <54713832+anjanasubhash@users.noreply.github.com>
2020-10-05 20:42:56 +0530

    Add files via upload

---
+++ Day3/q1.php/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+//error_reporting(0);
+
+$conn = mysqli_connect("localhost","root","") or die(mysqli_connect_error());
+
+//mysqli_select_db($conn,"workshop") or die(mysqli_error($conn));
+
+echo "Connected! <br/>"
+
+
+
+?>
